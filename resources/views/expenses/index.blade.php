3924800bc081c609a043ac4f4f4b38a3b39af5c6
Erik <erik_al1@hotmail.com>
2024-03-04 18:55:36 -0600

    feat: load data using excel file

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -2,6 +2,10 @@
 @section('content')
     <div id="expensesList" class="row justify-content-center">
         <div class="col-md-8">
+            <div class="text-end">
+                <button class="btn btn-primary my-2" @click="triggerFileInput">Cargar Excel</button>
+                <input class="d-none" type="file" id="fileInput" ref="fileInput" @change="handleFileUpload" accept=".xls,.xlsx,.xlsm,.xlsb,.xlt,.xltm,.xltx,.xla,.xlam,.xll,.xlw">
+            </div>
             <div class="card shadow border-0">
                 <div class="card-body">
                     <table class="table table-striped">
@@ -61,6 +65,22 @@
                             alert('Categoría actualizada');
                         }).catch(() => {
                             alert('No pudimos actualizar la categoría');
+                        })
+                },
+                triggerFileInput() {
+                    this.$refs.fileInput.click();
+                },
+                handleFileUpload() {
+                    let file = this.$refs.fileInput.files[0];
+                    let formData = new FormData();
+                    formData.append('file', file);
+
+                    axios.post('/api/v1/expenses/import', formData)
+                        .then(() => {
+                            alert('Estamos procesando tu archivo, te enviaremos un correo cuando esté listo. Puedes seguir trabajando en otras cosas mientras tanto.
+                            this.getExpenses();
+                        }).catch(() => {
+                            alert('No pudimos importar los gastos');
                         })
                 }
             },
