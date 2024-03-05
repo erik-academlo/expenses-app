c3504bb5043c011e83af77b5d08563ef525b77e5
Erik <erik_al1@hotmail.com>
2024-03-04 20:44:31 -0600

    refactor: login styles and remember old values for inputs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,30 +2,35 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card">
+            <div class="card shadow-sm border-0">
                 <div class="card-body">
-                    <h5 class="card-title">Login</h5>
+                    <h5 class="card-title">Inicio de sesión</h5>
+                    <hr>
                     @if ($errors->any())
                         <div class="alert alert-danger">
-                            <ul>
+                            <ul class="m-0">
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
                                 @endforeach
                             </ul>
                         </div>
                     @endif
-                    <hr>
                     <form method="post" action="/api/v1/login">
                         @csrf
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email address</label>
-                            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+                            <label for="email" class="form-label">Correo</label>
+                            <input type="email" name="email" class="form-control" id="email" value="{{ old('email') }}" required>
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password">
+                            <label for="password" class="form-label">Contraseña</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="text-center">
+                            <span>
+                                ¿No tienes cuenta? <a href="/auth/register">Regístrate</a>
+                            </span>
+                            <button type="submit" class="btn btn-primary d-block w-100 mt-2">Iniciar sesión</button>
+                        </div>
                     </form>
                 </div>
             </div>
